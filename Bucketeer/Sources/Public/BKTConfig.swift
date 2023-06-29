302db51e7b29583f563bdaeaadb363d0702f991e
kenji <duyhung71089@gmail.com>
2023-06-29 22:33:06 +0700

    feat: create BKTConfig Builder

--- Bucketeer/Sources/Public/BKTConfig.swift
+++ Bucketeer/Sources/Public/BKTConfig.swift
@@ -13,50 +13,132 @@ public struct BKTConfig {
     let logger: BKTLogger?
 }
 
-extension BKTConfig {
-    public init(
-        apiKey: String,
-        apiEndpoint: String,
-        featureTag: String,
-        eventsFlushInterval: Int64 = Constant.DEFAULT_FLUSH_INTERVAL_MILLIS,
-        eventsMaxQueueSize: Int = Constant.DEFAULT_MAX_QUEUE_SIZE,
-        pollingInterval: Int64 = Constant.DEFAULT_POLLING_INTERVAL_MILLIS,
-        backgroundPollingInterval: Int64 = Constant.DEFAULT_BACKGROUND_POLLING_INTERVAL_MILLIS,
-        appVersion: String,
-        logger: BKTLogger? = nil
-    ) throws {
-        guard !apiKey.isEmpty else {
+public class BKTConfigBuilder{
+    private(set) var apiKey: String?
+    private(set) var apiEndpoint: String?
+    private(set) var featureTag: String?
+    private(set) var eventsFlushInterval: Int64?
+    private(set) var eventsMaxQueueSize: Int?
+    private(set) var pollingInterval: Int64?
+    private(set) var backgroundPollingInterval: Int64?
+    private(set) var sdkVersion: String?
+    private(set) var appVersion: String?
+    private(set) var logger: BKTLogger?
+    
+    func with(apiKey: String?) -> BKTConfigBuilder {
+        self.apiKey = apiKey
+        return self
+    }
+    
+    func with(apiEndpoint: String?) -> BKTConfigBuilder {
+        self.apiEndpoint = apiEndpoint
+        return self
+    }
+    
+    func with(featureTag: String?) -> BKTConfigBuilder {
+        self.featureTag = featureTag
+        return self
+    }
+    
+    func with(eventsFlushInterval: Int64?) -> BKTConfigBuilder {
+        self.eventsFlushInterval = eventsFlushInterval
+        return self
+    }
+    
+    func with(eventsMaxQueueSize: Int?) -> BKTConfigBuilder {
+        self.eventsMaxQueueSize = eventsMaxQueueSize
+        return self
+    }
+    
+    func with(pollingInterval: Int64?) -> BKTConfigBuilder {
+        self.pollingInterval = pollingInterval
+        return self
+    }
+    
+    func with(backgroundPollingInterval: Int64?) -> BKTConfigBuilder {
+        self.backgroundPollingInterval = backgroundPollingInterval
+        return self
+    }
+    
+    func with(sdkVersion: String?) -> BKTConfigBuilder {
+        self.sdkVersion = sdkVersion
+        return self
+    }
+    
+    func with(appVersion: String?) -> BKTConfigBuilder {
+        self.appVersion = appVersion
+        return self
+    }
+    
+    func with(logger: BKTLogger?) -> BKTConfigBuilder {
+        self.logger = logger
+        return self
+    }
+    
+    func build() throws -> BKTConfig {
+        guard let apiKeyForSDK = apiKey, apiKeyForSDK.isNotEmpty() else {
             throw BKTError.illegalArgument(message: "apiKey is required")
         }
-        guard let apiEndpointURL = URL(string: apiEndpoint) else {
+        guard let endpoint = apiEndpoint, let apiEndpointURL = URL(string: endpoint) else {
             throw BKTError.illegalArgument(message: "endpoint is required")
         }
-        guard !featureTag.isEmpty else {
+        guard let tag = featureTag, tag.isNotEmpty() else {
             throw BKTError.illegalArgument(message: "featureTag is required")
         }
-        guard !appVersion.isEmpty else {
+        guard let version = appVersion, version.isNotEmpty() else {
             throw BKTError.illegalArgument(message: "appVersion is required")
         }
-
-        var pollingInterval = pollingInterval
+        
+        var pollingInterval : Int64 = pollingInterval ?? Constant.MINIMUM_POLLING_INTERVAL_MILLIS
         if pollingInterval < Constant.MINIMUM_POLLING_INTERVAL_MILLIS {
             logger?.warn(message: "pollingInterval: \(pollingInterval) is set but must be above \(Constant.MINIMUM_POLLING_INTERVAL_MILLIS)")
             pollingInterval = Constant.MINIMUM_POLLING_INTERVAL_MILLIS
         }
-        var backgroundPollingInterval = backgroundPollingInterval
+        var backgroundPollingInterval : Int64 = backgroundPollingInterval ?? Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS
         if backgroundPollingInterval < Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS {
             logger?.warn(message: "backgroundPollingInterval: \(backgroundPollingInterval) is set but must be above \(Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS)")
             backgroundPollingInterval = Constant.MINIMUM_BACKGROUND_POLLING_INTERVAL_MILLIS
         }
-        var eventsFlushInterval = eventsFlushInterval
+        var eventsFlushInterval: Int64 = eventsFlushInterval ?? Constant.DEFAULT_FLUSH_INTERVAL_MILLIS
         if eventsFlushInterval < Constant.MINIMUM_FLUSH_INTERVAL_MILLIS {
             logger?.warn(message: "eventsFlushInterval: \(eventsFlushInterval) is set but must be above \(Constant.MINIMUM_FLUSH_INTERVAL_MILLIS)")
             eventsFlushInterval = Constant.DEFAULT_FLUSH_INTERVAL_MILLIS
         }
+        
+        return try BKTConfig.init(
+            apiKey: apiKeyForSDK,
+            apiEndpoint: apiEndpointURL,
+            featureTag: tag,
+            eventsFlushInterval: eventsFlushInterval,
+            eventsMaxQueueSize: eventsMaxQueueSize ?? Constant.DEFAULT_MAX_QUEUE_SIZE,
+            pollingInterval: pollingInterval,
+            backgroundPollingInterval: backgroundPollingInterval,
+            appVersion: version)
+    }
+}
 
+fileprivate extension String {
+    func isNotEmpty() -> Bool {
+        // We will not check all case
+        return count > 0
+    }
+}
+
+fileprivate extension BKTConfig {
+    init(
+        apiKey: String,
+        apiEndpoint: URL,
+        featureTag: String,
+        eventsFlushInterval: Int64,
+        eventsMaxQueueSize: Int,
+        pollingInterval: Int64,
+        backgroundPollingInterval: Int64,
+        appVersion: String,
+        logger: BKTLogger? = nil
+    ) throws {
         self = BKTConfig(
             apiKey: apiKey,
-            apiEndpoint: apiEndpointURL,
+            apiEndpoint: apiEndpoint,
             featureTag: featureTag,
             eventsFlushInterval: eventsFlushInterval,
             eventsMaxQueueSize: eventsMaxQueueSize,
