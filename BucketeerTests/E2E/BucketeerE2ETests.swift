881de7fbd575fb1c01946ccbb62c13a179deea18
duyhungtnn <duyhung71089@gmail.com>
2023-07-26 19:54:44 +0700

    chore: change to throw an exception instead of using fatalError from BKTClient.shared (#18)

--- BucketeerTests/E2E/BucketeerE2ETests.swift
+++ BucketeerTests/E2E/BucketeerE2ETests.swift
@@ -25,140 +25,198 @@ final class BucketeerE2ETests: XCTestCase {
         try await super.tearDown()
 
         try await BKTClient.shared.flush()
-        BKTClient.destroy()
+        try BKTClient.destroy()
         UserDefaults.standard.removeObject(forKey: "bucketeer_user_evaluations_id")
         try FileManager.default.removeItem(at: .database)
     }
 
     func testStringVariation() {
-        let client = BKTClient.shared
-        XCTAssertEqual(client.stringVariation(featureId: FEATURE_ID_STRING, defaultValue: ""), "value-1")
+        do {
+            let client = try BKTClient.shared
+            XCTAssertEqual(client.stringVariation(featureId: FEATURE_ID_STRING, defaultValue: ""), "value-1")
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testStringVariationDetail() {
-        let client = BKTClient.shared
-        let actual = client.evaluationDetails(featureId: FEATURE_ID_STRING)
+        do {
+            let client = try BKTClient.shared
+            let actual = client.evaluationDetails(featureId: FEATURE_ID_STRING)
 
-        assertEvaluation(actual: actual, expected: .init(
-            id: "feature-ios-e2e-string:3:bucketeer-ios-user-id-1",
-            featureId: FEATURE_ID_STRING,
-            featureVersion: 3,
-            variationId: "349ed945-d2f9-4d04-8e83-82344cffd1ec",
-            variationName: "variation 1",
-            variationValue: "value-1",
-            reason: .default
-        ))
+            assertEvaluation(actual: actual, expected: .init(
+                id: "feature-ios-e2e-string:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_STRING,
+                featureVersion: 3,
+                variationId: "349ed945-d2f9-4d04-8e83-82344cffd1ec",
+                variationName: "variation 1",
+                variationValue: "value-1",
+                reason: .default
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testIntVariation() {
-        let client = BKTClient.shared
-        XCTAssertEqual(client.intVariation(featureId: FEATURE_ID_INT, defaultValue: 0), 10)
+        do {
+            let client = try BKTClient.shared
+            XCTAssertEqual(client.intVariation(featureId: FEATURE_ID_INT, defaultValue: 0), 10)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testIntVariationDetail() {
-        let client = BKTClient.shared
-        let actual = client.evaluationDetails(featureId: FEATURE_ID_INT)
+        do {
+            let client = try BKTClient.shared
+            let actual = client.evaluationDetails(featureId: FEATURE_ID_INT)
 
-        assertEvaluation(actual: actual, expected: .init(
-            id: "feature-ios-e2e-integer:3:bucketeer-ios-user-id-1",
-            featureId: FEATURE_ID_INT,
-            featureVersion: 3,
-            variationId: "9c5fd2d2-d587-4ba2-8de2-0fc9454d564e",
-            variationName: "variation 10",
-            variationValue: "10",
-            reason: .default
-        ))
+            assertEvaluation(actual: actual, expected: .init(
+                id: "feature-ios-e2e-integer:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_INT,
+                featureVersion: 3,
+                variationId: "9c5fd2d2-d587-4ba2-8de2-0fc9454d564e",
+                variationName: "variation 10",
+                variationValue: "10",
+                reason: .default
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testDoubleVariation() {
-        let client = BKTClient.shared
-        XCTAssertEqual(client.doubleVariation(featureId: FEATURE_ID_DOUBLE, defaultValue: 0.1), 2.1)
+        do {
+            let client = try BKTClient.shared
+            XCTAssertEqual(client.doubleVariation(featureId: FEATURE_ID_DOUBLE, defaultValue: 0.1), 2.1)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testDoubleVariationDetail() async throws {
-        let client = BKTClient.shared
-        let actual = client.evaluationDetails(featureId: FEATURE_ID_DOUBLE)
+        do {
+            let client = try BKTClient.shared
+            let actual = client.evaluationDetails(featureId: FEATURE_ID_DOUBLE)
 
-        assertEvaluation(actual: actual, expected: .init(
-            id: "feature-ios-e2e-double:3:bucketeer-ios-user-id-1",
-            featureId: FEATURE_ID_DOUBLE,
-            featureVersion: 3,
-            variationId: "38078d8f-c6eb-4b93-9d58-c3e57010983f",
-            variationName: "variation 2.1",
-            variationValue: "2.1",
-            reason: .default
-        ))
+            assertEvaluation(actual: actual, expected: .init(
+                id: "feature-ios-e2e-double:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_DOUBLE,
+                featureVersion: 3,
+                variationId: "38078d8f-c6eb-4b93-9d58-c3e57010983f",
+                variationName: "variation 2.1",
+                variationValue: "2.1",
+                reason: .default
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testBoolVariation() {
-        let client = BKTClient.shared
-        XCTAssertEqual(client.boolVariation(featureId: FEATURE_ID_BOOLEAN, defaultValue: false), true)
+        do {
+            let client = try BKTClient.shared
+            let actual = client.evaluationDetails(featureId: FEATURE_ID_BOOLEAN)
+
+            assertEvaluation(actual: actual, expected: .init(
+                id: "feature-ios-e2e-bool:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_BOOLEAN,
+                featureVersion: 3,
+                variationId: "4f9e0f88-e053-42a9-93e1-95d407f67021",
+                variationName: "variation true",
+                variationValue: "true",
+                reason: .default
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testBoolVariationDetail() {
-        let client = BKTClient.shared
-        let actual = client.evaluationDetails(featureId: FEATURE_ID_BOOLEAN)
+        do {
+            let client = try BKTClient.shared
+            let actual = client.evaluationDetails(featureId: FEATURE_ID_BOOLEAN)
 
-        assertEvaluation(actual: actual, expected: .init(
-            id: "feature-ios-e2e-bool:3:bucketeer-ios-user-id-1",
-            featureId: FEATURE_ID_BOOLEAN,
-            featureVersion: 3,
-            variationId: "4f9e0f88-e053-42a9-93e1-95d407f67021",
-            variationName: "variation true",
-            variationValue: "true",
-            reason: .default
-        ))
+            assertEvaluation(actual: actual, expected: .init(
+                id: "feature-ios-e2e-bool:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_BOOLEAN,
+                featureVersion: 3,
+                variationId: "4f9e0f88-e053-42a9-93e1-95d407f67021",
+                variationName: "variation true",
+                variationValue: "true",
+                reason: .default
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testJSONVariation() {
-        let client = BKTClient.shared
-        let json = client.jsonVariation(featureId: FEATURE_ID_JSON, defaultValue: [:])
-        XCTAssertEqual(json as? [String: String], ["key": "value-1"])
+        do {
+            let client = try BKTClient.shared
+            let json = client.jsonVariation(featureId: FEATURE_ID_JSON, defaultValue: [:])
+            XCTAssertEqual(json as? [String: String], ["key": "value-1"])
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testJSONVariationDetail() {
-        let client = BKTClient.shared
-        let actual = client.evaluationDetails(featureId: FEATURE_ID_JSON)
+        do {
+            let client = try BKTClient.shared
+            let actual = client.evaluationDetails(featureId: FEATURE_ID_JSON)
 
-        assertEvaluation(actual: actual, expected: .init(
-            id: "feature-ios-e2e-json:3:bucketeer-ios-user-id-1",
-            featureId: FEATURE_ID_JSON,
-            featureVersion: 3,
-            variationId: "06f5be6b-0c79-431f-a057-822babd9d3eb",
-            variationName: "variation 1",
-            variationValue: "{ \"key\": \"value-1\" }",
-            reason: .default
-        ))
+            assertEvaluation(actual: actual, expected: .init(
+                id: "feature-ios-e2e-json:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_JSON,
+                featureVersion: 3,
+                variationId: "06f5be6b-0c79-431f-a057-822babd9d3eb",
+                variationName: "variation 1",
+                variationValue: "{ \"key\": \"value-1\" }",
+                reason: .default
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testEvaluationUpdateFlow() async throws {
-        let client = BKTClient.shared
-        XCTAssertEqual(client.stringVariation(featureId: FEATURE_ID_STRING, defaultValue: ""), "value-1")
+        do {
+            let client = try BKTClient.shared
+            XCTAssertEqual(client.stringVariation(featureId: FEATURE_ID_STRING, defaultValue: ""), "value-1")
 
-        client.updateUserAttributes(attributes: ["app_version": "0.0.1"])
+            client.updateUserAttributes(attributes: ["app_version": "0.0.1"])
 
-        try await client.fetchEvaluations(timeoutMillis: nil)
-        XCTAssertEqual(client.stringVariation(featureId: FEATURE_ID_STRING, defaultValue: ""), "value-2")
+            try await client.fetchEvaluations(timeoutMillis: nil)
+            XCTAssertEqual(client.stringVariation(featureId: FEATURE_ID_STRING, defaultValue: ""), "value-2")
 
-        let details = client.evaluationDetails(featureId: FEATURE_ID_STRING)
-        assertEvaluation(actual: details, expected: .init(
-            id: "feature-ios-e2e-string:3:bucketeer-ios-user-id-1",
-            featureId: FEATURE_ID_STRING,
-            featureVersion: 3,
-            variationId: "b4931643-e82f-4079-bd3c-aed02852cdd6",
-            variationName: "variation 2",
-            variationValue: "value-2",
-            reason: .rule
-        ))
+            let details = client.evaluationDetails(featureId: FEATURE_ID_STRING)
+            assertEvaluation(actual: details, expected: .init(
+                id: "feature-ios-e2e-string:3:bucketeer-ios-user-id-1",
+                featureId: FEATURE_ID_STRING,
+                featureVersion: 3,
+                variationId: "b4931643-e82f-4079-bd3c-aed02852cdd6",
+                variationName: "variation 2",
+                variationValue: "value-2",
+                reason: .rule
+            ))
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 
     func testTrack() async throws {
-        let client = BKTClient.shared
-        client.assert(expectedEventCount: 2)
-        client.track(goalId: GOAL_ID, value: GOAL_VALUE)
-        try await Task.sleep(nanoseconds: 1_000_000)
-        client.assert(expectedEventCount: 3)
-        try await client.flush()
-        client.assert(expectedEventCount: 0)
+        do {
+            let client = try BKTClient.shared
+            client.assert(expectedEventCount: 2)
+            client.track(goalId: GOAL_ID, value: GOAL_VALUE)
+            try await Task.sleep(nanoseconds: 1_000_000)
+            client.assert(expectedEventCount: 3)
+            try await client.flush()
+            client.assert(expectedEventCount: 0)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
     }
 }
