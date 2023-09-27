0eb8b43c08c7bc4a9feda77b9f42fcf138508110
duyhungtnn <duyhung71089@gmail.com>
2023-09-27 12:23:15 +0700

    chore: remove unnecessary comment

--- Bucketeer/Sources/Internal/Remote/RequestBody/GetEvaluationsRequestBody.swift
+++ Bucketeer/Sources/Internal/Remote/RequestBody/GetEvaluationsRequestBody.swift
@@ -6,7 +6,5 @@ struct GetEvaluationsRequestBody: Codable {
     let userEvaluationsId: String
     let sourceId: SourceID
     let userEvaluationCondition: UserEvaluationCondition
-    // noted: we didn't set the default value to prevent the warning about Decodeable will not work
-    // error message "Immutable property will not be decoded because it is declared with an initial value which cannot be overwritten"
     let sdkVersion: String
 }
