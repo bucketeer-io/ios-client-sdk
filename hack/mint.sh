28fab46a48b708f7986a3b8ee25e6394fdedf5d4
kakcy <kakinoki_yoshifumi@cyberagent.co.jp>
2023-10-26 19:04:17 +0900

    ci: export mint link path only local run

--- hack/mint.sh
+++ hack/mint.sh
@@ -3,13 +3,14 @@
 MINT_LINK_PATH_AT_INSTALL="mint/bin"
 
 function bootstrap_mint() {
-    PROJECT_DIR=$(xcodebuild -showBuildSettings | grep "PROJECT_DIR = .*" | sed "s/PROJECT_DIR = //g" | sed "s/ //g")
-    echo "PROJECT_DIR=$PROJECT_DIR"
+    if [ "$CI" = "" ]; then
+        PROJECT_DIR=$(xcodebuild -showBuildSettings | grep "PROJECT_DIR = .*" | sed "s/PROJECT_DIR = //g" | sed "s/ //g")
+        export MINT_LINK_PATH="$PROJECT_DIR/$MINT_LINK_PATH_AT_INSTALL"
+    fi
 
-    export MINT_LINK_PATH="$PROJECT_DIR/$MINT_LINK_PATH_AT_INSTALL"
-
-    command_output=$(mint bootstrap -l --overwrite y 2>&1)
+    command_output=$(mint bootstrap --link --overwrite y 2>&1)
     exit_status=$?
+
     if [ $exit_status -ne 0 ]; then
        echo "mint bootstrap was error. Please make install-mint. $command_output"
        exit 1
