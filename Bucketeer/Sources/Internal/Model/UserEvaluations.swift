f270c7c0840d0b7d8c825637a728ebecade69243
kenji <duyhung71089@gmail.com>
2023-10-26 21:56:30 +0700

    chore: remove used comment

--- Bucketeer/Sources/Internal/Model/UserEvaluations.swift
+++ Bucketeer/Sources/Internal/Model/UserEvaluations.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 struct UserEvaluations: Hashable, Codable {
-    // note: should check if we could using `let`
     let id: String
     let evaluations: [Evaluation]
     let createdAt: String
