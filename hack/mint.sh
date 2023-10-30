576719f0bf5e672f6e845b8f3b329f25fd123fd4
kakcy <kakinoki_yoshifumi@cyberagent.co.jp>
2023-10-30 19:13:59 +0900

    ci: change the path required for mint due to differences in CI and other conditions

--- hack/mint.sh
+++ hack/mint.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+MINT_LIBRARY_PATH_AT_INSTALL="mint/lib"
 MINT_LINK_PATH_AT_INSTALL="mint/bin"
 
 function bootstrap_mint() {
     if [ "$CI" = "" ]; then
-        PROJECT_DIR=$(xcodebuild -showBuildSettings | grep "PROJECT_DIR = .*" | sed "s/PROJECT_DIR = //g" | sed "s/ //g")
-        export MINT_LINK_PATH="$PROJECT_DIR/$MINT_LINK_PATH_AT_INSTALL"
+        export MINT_LINK_PATH="$MINT_LINK_PATH_AT_INSTALL"
+        export MINT_PATH="$MINT_LIBRARY_PATH_AT_INSTALL"
+    else
+        export MINT_LINK_PATH=".$MINT_LINK_PATH_AT_INSTALL"
+        export MINT_PATH=".$MINT_LIBRARY_PATH_AT_INSTALL"
     fi
 
     command_output=$(mint bootstrap --link --overwrite y 2>&1)
     exit_status=$?
 
     if [ $exit_status -ne 0 ]; then
-       echo "mint bootstrap was error. Please make install-mint. $command_output"
-       exit 1
+        echo "mint bootstrap was error. Please make install-mint. $command_output"
+        exit 1
     fi
 }
 
