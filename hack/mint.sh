8b5d5e7cdfe747ba9ac82a91b4b2cbf1b6590d80
kakcy <kakinoki_yoshifumi@cyberagent.co.jp>
2023-10-30 20:55:13 +0900

    ci: fix to install mint at can't search it

--- hack/mint.sh
+++ hack/mint.sh
@@ -12,11 +12,19 @@ function bootstrap_mint() {
         export MINT_PATH=".$MINT_LIBRARY_PATH_AT_INSTALL"
     fi
 
+    result_search_mint=$(brew list | grep mint 2>&1)
+    exit_status=$?
+
+    if [ $exit_status -ne 0 ] || [result_search_mint=""]; then
+        echo "mint doesn't install. $command_output"
+        install_mint
+    fi
+
     command_output=$(mint bootstrap --link --overwrite y 2>&1)
     exit_status=$?
 
     if [ $exit_status -ne 0 ]; then
-        echo "mint bootstrap was error. Please make install-mint. $command_output"
+        echo "mint bootstrap was error. $command_output"
         exit 1
     fi
 }
