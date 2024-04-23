374218ad331bdf18edbf42110a2ecc4ff7742a06
duyhungtnn <duyhung71089@gmail.com>
2024-04-23 12:06:36 +0700

    chore: add the source ID to the register events API request (#84)

--- Bucketeer/Sources/Internal/Remote/RequestBody/RegisterEventsRequestBody.swift
+++ Bucketeer/Sources/Internal/Remote/RequestBody/RegisterEventsRequestBody.swift
@@ -1,6 +1,16 @@
 import Foundation
 
 struct RegisterEventsRequestBody: Codable {
+    internal init(events: [Event] = [],
+                  sdkVersion: String = Version.current,
+                  sourceId: SourceID = SourceID.ios
+    ) {
+        self.events = events
+        self.sdkVersion = sdkVersion
+        self.sourceId = sourceId
+    }
+
     let events: [Event]
     let sdkVersion: String
+    let sourceId: SourceID
 }
