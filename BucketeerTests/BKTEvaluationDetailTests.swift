d170ebe84a91bcc0b47b31cbfe310074d5e7cadd
duyhungtnn <duyhung71089@gmail.com>
2024-06-25 18:12:01 +0700

    test: for Reason.fromString

--- BucketeerTests/BKTEvaluationDetailTests.swift
+++ BucketeerTests/BKTEvaluationDetailTests.swift
@@ -179,55 +179,68 @@ final class BKTEvaluationDetailTests: XCTestCase {
 
     func testCreateDefaultInstance() throws {
         XCTAssertEqual(BKTEvaluationDetails<Int>.newDefaultInstance(featureId: "1", userId: "2", defaultValue: 2),
-               BKTEvaluationDetails(
-                featureId: "1",
-                featureVersion: 0,
-                userId: "2",
-                variationId: "",
-                variationName: "",
-                variationValue: 2,
-                reason: .client)
+                       BKTEvaluationDetails(
+                        featureId: "1",
+                        featureVersion: 0,
+                        userId: "2",
+                        variationId: "",
+                        variationName: "",
+                        variationValue: 2,
+                        reason: .client)
         )
         XCTAssertEqual(BKTEvaluationDetails<String>.newDefaultInstance(featureId: "2", userId: "3", defaultValue: "2"),
-               BKTEvaluationDetails(
-                featureId: "2",
-                featureVersion: 0,
-                userId: "3",
-                variationId: "",
-                variationName: "",
-                variationValue: "2",
-                reason: .client)
+                       BKTEvaluationDetails(
+                        featureId: "2",
+                        featureVersion: 0,
+                        userId: "3",
+                        variationId: "",
+                        variationName: "",
+                        variationValue: "2",
+                        reason: .client)
         )
         XCTAssertEqual(BKTEvaluationDetails<Double>.newDefaultInstance(featureId: "1", userId: "2", defaultValue: 2.0),
-               BKTEvaluationDetails(
-                featureId: "1",
-                featureVersion: 0,
-                userId: "2",
-                variationId: "",
-                variationName: "",
-                variationValue: 2.0,
-                reason: .client)
+                       BKTEvaluationDetails(
+                        featureId: "1",
+                        featureVersion: 0,
+                        userId: "2",
+                        variationId: "",
+                        variationName: "",
+                        variationValue: 2.0,
+                        reason: .client)
         )
         XCTAssertEqual(BKTEvaluationDetails<Bool>.newDefaultInstance(featureId: "11", userId: "22", defaultValue: true),
-               BKTEvaluationDetails(
-                featureId: "11",
-                featureVersion: 0,
-                userId: "22",
-                variationId: "",
-                variationName: "",
-                variationValue: true,
-                reason: .client)
+                       BKTEvaluationDetails(
+                        featureId: "11",
+                        featureVersion: 0,
+                        userId: "22",
+                        variationId: "",
+                        variationName: "",
+                        variationValue: true,
+                        reason: .client)
         )
         XCTAssertEqual(BKTEvaluationDetails<[String: AnyHashable]>.newDefaultInstance(featureId: "11", userId: "22", defaultValue: ["key":"value"]),
-               BKTEvaluationDetails(
-                featureId: "11",
-                featureVersion: 0,
-                userId: "22",
-                variationId: "",
-                variationName: "",
-                variationValue: ["key":"value"],
-                reason: .client)
+                       BKTEvaluationDetails(
+                        featureId: "11",
+                        featureVersion: 0,
+                        userId: "22",
+                        variationId: "",
+                        variationName: "",
+                        variationValue: ["key":"value"],
+                        reason: .client)
         )
+    }
+
+    func testFromStringWithValidValues() {
+        XCTAssertEqual(BKTEvaluationDetails<String>.Reason.fromString(value: "TARGET"), .target)
+        XCTAssertEqual(BKTEvaluationDetails<Bool>.Reason.fromString(value: "RULE"), .rule)
+        XCTAssertEqual(BKTEvaluationDetails<Int>.Reason.fromString(value: "DEFAULT"), .default)
+        XCTAssertEqual(BKTEvaluationDetails<Double>.Reason.fromString(value: "CLIENT"), .client)
+        XCTAssertEqual(BKTEvaluationDetails<Bool>.Reason.fromString(value: "OFF_VARIATION"), .offVariation)
+        XCTAssertEqual(BKTEvaluationDetails<Int>.Reason.fromString(value: "PREREQUISITE"), .prerequisite)
+    }
+
+    func testFromStringWithInvalidValue() {
+        XCTAssertEqual(BKTEvaluationDetails<Int>.Reason.fromString(value: "INVALID"), .client)
     }
 }
 // swiftlint:enable function_body_length
