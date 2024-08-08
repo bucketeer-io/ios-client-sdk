3d3bad197473379bd14f3e323b5315a158fb0e59
duyhungtnn <duyhung71089@gmail.com>
2024-08-08 10:47:29 +0700

    tests: update BKTValueTests

--- BucketeerTests/BKTValueTests.swift
+++ BucketeerTests/BKTValueTests.swift
@@ -2,106 +2,92 @@ import Foundation
 import XCTest
 @testable import Bucketeer
 
+class JSONSerializationTests: XCTestCase {
+
+    func testJSONSerializationRemoveTrailingZeroesFromDoubles() {
+        // Given
+        let jsonObject: [String: Any] = ["value": 1.0]
+
+        // When
+        let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+
+        // Then
+        let expectedJsonString = "{\n  \"value\" : 1\n}"
+        XCTAssertEqual(jsonString, expectedJsonString)
+    }
+}
+
+class BKTValueEncodingTests: XCTestCase {
+
+    struct EncodeTestCase {
+        let value: BKTValue
+        let expectedJsonString: String
+    }
+
+    let testCases: [EncodeTestCase] = [
+        EncodeTestCase(value: .string("test string"), expectedJsonString: "\"test string\""),
+        EncodeTestCase(value: .string(""), expectedJsonString: "\"\""),
+        EncodeTestCase(value: .integer(123), expectedJsonString: "123"),
+        EncodeTestCase(value: .double(123.456), expectedJsonString: "123.456"),
+        EncodeTestCase(value: .double(123.0), expectedJsonString: "123"),
+        EncodeTestCase(value: .boolean(true), expectedJsonString: "true"),
+        EncodeTestCase(value: .dictionary(["key": .string("value")]), expectedJsonString: "{\"key\":\"value\"}"),
+        EncodeTestCase(value: .list([.string("value1"), .string("value2")]), expectedJsonString: "[\"value1\",\"value2\"]"),
+        EncodeTestCase(value: .null, expectedJsonString: "null")
+    ]
+
+    func testEncode() throws {
+        for testCase in testCases {
+            let encoded = try JSONEncoder().encode(testCase.value)
+            let jsonString = String(data: encoded, encoding: .utf8)!
+            XCTAssertEqual(jsonString, testCase.expectedJsonString)
+        }
+    }
+}
+
+class BKTValueDecodeTests: XCTestCase {
+
+    struct DecodeTestCase {
+        let json: String
+        let expected: BKTValue
+    }
+
+    func testDecode() throws {
+        let testCases: [DecodeTestCase] = [
+            DecodeTestCase(json: "\"test string\"", expected: .string("test string")),
+            DecodeTestCase(json: "null", expected: .null),
+            DecodeTestCase(json: "123", expected: .integer(123)),
+            DecodeTestCase(json: "123.456", expected: .double(123.456)),
+            DecodeTestCase(json: "123.0", expected: .integer(123)),
+            DecodeTestCase(json: "123.00", expected: .integer(123)),
+            DecodeTestCase(json: "true", expected: .boolean(true)),
+            DecodeTestCase(json: "false", expected: .boolean(false)),
+            DecodeTestCase(json: "\"true\"", expected: .string("true")),
+            DecodeTestCase(json: "{\"key\": \"value\"}", expected: .dictionary(["key": .string("value")]))
+        ]
+
+        for testCase in testCases {
+            let data = testCase.json.data(using: .utf8)!
+            let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
+            XCTAssertEqual(decoded, testCase.expected, "Failed to decode \(testCase.json)")
+        }
+    }
+
+    func testDecodeEmptyString() throws {
+        let jsonEmptyString = ""
+        let dataEmptyString = jsonEmptyString.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(BKTValue.self, from: dataEmptyString))
+    }
+
+    func testDecodeInvalidString() throws {
+        let jsonInvalidString = "test string"
+        let dataInvalidString = jsonInvalidString.data(using: .utf8)!
+        XCTAssertThrowsError(try JSONDecoder().decode(BKTValue.self, from: dataInvalidString))
+    }
+}
+
 final class BKTValueTests: XCTestCase {
-
-    func testDecodeString() throws {
-        let json = "\"test string\""
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .string("test string"))
-    }
-
-    func testDecodeInteger() throws {
-        let json = "123"
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .integer(123))
-    }
-
-    func testDecodeDouble() throws {
-        let json = "123.456"
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .double(123.456))
-    }
-
-    func testDecodeBoolean() throws {
-        let json = "true"
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .boolean(true))
-    }
-
-    func testDecodeDictionary() throws {
-        let json = "{\"key\": \"value\"}"
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .dictionary(["key": .string("value")]))
-    }
-
-    func testDecodeList() throws {
-        let json = "[\"value1\", \"value2\"]"
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .list([.string("value1"), .string("value2")]))
-    }
-
-    func testDecodeNull() throws {
-        let json = "null"
-        let data = json.data(using: .utf8)!
-        let decoded = try JSONDecoder().decode(BKTValue.self, from: data)
-        XCTAssertEqual(decoded, .null)
-    }
-
-    func testEncodeString() throws {
-        let value = BKTValue.string("test string")
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "\"test string\"")
-    }
-
-    func testEncodeInteger() throws {
-        let value = BKTValue.integer(123)
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "123")
-    }
-
-    func testEncodeDouble() throws {
-        let value = BKTValue.double(123.456)
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "123.456")
-    }
-
-    func testEncodeBoolean() throws {
-        let value = BKTValue.boolean(true)
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "true")
-    }
-
-    func testEncodeDictionary() throws {
-        let value = BKTValue.dictionary(["key": .string("value")])
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "{\"key\":\"value\"}")
-    }
-
-    func testEncodeList() throws {
-        let value = BKTValue.list([.string("value1"), .string("value2")])
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "[\"value1\",\"value2\"]")
-    }
-
-    func testEncodeNull() throws {
-        let value = BKTValue.null
-        let encoded = try JSONEncoder().encode(value)
-        let jsonString = String(data: encoded, encoding: .utf8)!
-        XCTAssertEqual(jsonString, "null")
-    }
-
     func testAsBoolean() {
         XCTAssertEqual(BKTValue.boolean(true).asBoolean(), true)
         XCTAssertNil(BKTValue.string("string").asBoolean())
@@ -162,13 +148,18 @@ final class BKTValueTests: XCTestCase {
         XCTAssertNil(BKTValue.null.asDictionary())
     }
 
-    func testDecodeJsonString() throws {
+    func testGetVariationBKTValue() throws {
         XCTAssertEqual("".getVariationBKTValue(logger: nil), .string(""))
+        XCTAssertEqual("null".getVariationBKTValue(logger: nil), .string("null"))
         XCTAssertEqual("test".getVariationBKTValue(logger: nil), .string("test"))
+        XCTAssertEqual("test value".getVariationBKTValue(logger: nil), .string("test value"))
+        XCTAssertEqual("\"test value\"".getVariationBKTValue(logger: nil), .string("test value"))
         XCTAssertEqual("true".getVariationBKTValue(logger: nil), .boolean(true))
         XCTAssertEqual("false".getVariationBKTValue(logger: nil), .boolean(false))
         XCTAssertEqual("1".getVariationBKTValue(logger: nil), .integer(1))
+        XCTAssertEqual("1.0".getVariationBKTValue(logger: nil), .integer(1))
         XCTAssertEqual("1.2".getVariationBKTValue(logger: nil), .double(1.2))
+        XCTAssertEqual("1.234".getVariationBKTValue(logger: nil), .double(1.234))
 
         let dictionaryJSONText = """
 {
