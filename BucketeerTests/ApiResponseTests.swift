99ac344f8f0cdd91fe596bd15e40bc5b28cf34d9
duyhungtnn <duyhung71089@gmail.com>
2025-02-13 17:36:35 +0700

    test: add test to check if we could handle new values

--- BucketeerTests/ApiResponseTests.swift
+++ BucketeerTests/ApiResponseTests.swift
@@ -1,1 +1,27 @@
+import XCTest
+@testable import Bucketeer
 
+class ReasonTypeDecodingTests: XCTestCase {
+    func testDecodeValidAndInvalidReasonType() throws {
+        let json = """
+        [
+            "TARGET",
+            "RULE",
+            "OFF_VARIATION",
+            "PREREQUISITE",
+            "INVALID_VALUE",
+        ]
+        """.data(using: .utf8)!
+
+        let decoder = JSONDecoder()
+        let decoded = try decoder.decode([ReasonType].self, from: json)
+
+        XCTAssertEqual(decoded, [
+            .target,
+            .rule,
+            .default, // "INVALID_VALUE"
+            .offVariation,
+            .prerequisite
+        ])
+    }
+}
