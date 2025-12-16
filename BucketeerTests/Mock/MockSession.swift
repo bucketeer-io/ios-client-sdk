78960f053a51dfef2532142bf79036675374e6a0
duyhungtnn <duyhung71089@gmail.com>
2025-12-16 15:36:42 +0700

    chore: add test for retriable API call

--- BucketeerTests/Mock/MockSession.swift
+++ BucketeerTests/Mock/MockSession.swift
@@ -1,6 +1,14 @@
 import Foundation
 @testable import Bucketeer
 
+class MockSessionCounter {
+    var count: Int = 0
+
+    func increment() {
+        count += 1
+    }
+}
+
 struct MockSession: Session {
     var configuration: URLSessionConfiguration = .default
     var requestHandler: ((URLRequest) -> Void)?
@@ -9,11 +17,21 @@ struct MockSession: Session {
     var error: Error?
     let networkQueue = DispatchQueue(label: "io.bucketeer.concurrentQueue.network", attributes: .concurrent)
     var invalidateAndCancelHandler: (() -> Void)?
+    var responseProvider: ((URLRequest, Int) -> MockResponseData)?
+    var taskCounter: MockSessionCounter = MockSessionCounter()
 
     func task(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
         networkQueue.async {
+            taskCounter.increment()
             requestHandler?(request)
             networkQueue.asyncAfter(deadline: .now() + 0.1) {
+                if let responseProvider = self.responseProvider {
+                    // Dynamic response
+                    let responseData = responseProvider(request, taskCounter.count)
+                    completionHandler(responseData.data, responseData.response, responseData.error)
+                    return
+                }
+                // Fixed response
                 completionHandler(data, response, error)
             }
         }
@@ -22,4 +40,10 @@ struct MockSession: Session {
     func invalidateAndCancel() {
         invalidateAndCancelHandler?()
     }
+}
+
+struct MockResponseData {
+    let data: Data?
+    let response: HTTPURLResponse?
+    let error: Error?
 }
